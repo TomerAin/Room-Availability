7f2dbfd9f51fbcfa63b6cc7b4b76694b960d656c
TomerAin <tomerainhoren@gmail.com>
2025-04-27 14:23:04 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: "sans-serif" }}>
+      <h1>חדר פנוי בשניים 3?</h1>
+      <p>זהו אבטיפוס ראשוני לאפליקציה לשיתוף חדרים בין עובדים.</p>
+    </div>
+  );
+}
+
+export default App;

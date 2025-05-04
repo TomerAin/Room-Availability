264047df27ca0b12f3f9eaddef0aa5872487abbe
TomerAin <tomerainhoren@gmail.com>
2025-05-04 09:40:00 +0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6qisQGd2zdLN_wylOmLXE3M1Dpqte9_Q",
+  authDomain: "room-scheduler-579ef.firebaseapp.com",
+  databaseURL: "https://room-scheduler-579ef-default-rtdb.firebaseio.com",
+  projectId: "room-scheduler-579ef",
+  storageBucket: "room-scheduler-579ef.appspot.com",
+  messagingSenderId: "231957301822",
+  appId: "1:231957301822:web:1f55919f6eebc1a6615f6f"
+};

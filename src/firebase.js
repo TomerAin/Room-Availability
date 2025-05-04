3be858d39d6e72b1c2ad36a2400cc09dc63c9291
TomerAin <tomerainhoren@gmail.com>
2025-05-04 15:01:32 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA6qisQGd2zdLN_wylOmLXE3M1Dpqte9_Q",
   authDomain: "room-scheduler-579ef.firebaseapp.com",
-  databaseURL: "https://room-scheduler-579ef-default-rtdb.firebaseio.com",
+  databaseURL: "https://room-scheduler-579ef-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "room-scheduler-579ef",
-  storageBucket: "room-scheduler-579ef.appspot.com",
+  storageBucket: "room-scheduler-579ef.firebasestorage.app",
   messagingSenderId: "231957301822",
-  appId: "1:231957301822:web:1f55919f6eebc1a6615f6f"
+  appId: "1:231957301822:web:8b4a882f66464f34615f6f"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

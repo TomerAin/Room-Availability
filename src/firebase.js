89293026efc952a4a17d0e13791fb92da4915528
TomerAin <tomerainhoren@gmail.com>
2025-05-05 14:38:06 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,32 @@
-// src/firebase.js
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref, set, onValue } from "firebase/database";
 
-// הגדרות החיבור לפרויקט Firebase שלך
 const firebaseConfig = {
   apiKey: "AIzaSyA6qisQGd2zdLN_wylOmLXE3M1Dpqte9_Q",
   authDomain: "room-scheduler-579ef.firebaseapp.com",
   databaseURL: "https://room-scheduler-579ef-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "room-scheduler-579ef",
-  storageBucket: "room-scheduler-579ef.appspot.com",
+  storageBucket: "room-scheduler-579ef.firebasestorage.app",
   messagingSenderId: "231957301822",
   appId: "1:231957301822:web:8b4a882f66464f34615f6f"
 };
 
-// אתחול Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-export { database };
+// שמירת נתונים
+function saveData(path, data) {
+  return set(ref(database, path), data);
+}
+
+// האזנה לשינויים
+function subscribeToData(path, callback) {
+  const dataRef = ref(database, path);
+  onValue(dataRef, (snapshot) => {
+    const data = snapshot.val();
+    callback(data);
+  });
+}
+
+export { saveData, subscribeToData };
